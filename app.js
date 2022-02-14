0eee1aa0efef0f987c1250f270570570371bac9d
AronTMG64 <a.grankulla@optistud.fi>
2022-02-14 12:31:59 +0200

    ordering system progress

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
+localStorage.getItem('pepperoniPrice')
+localStorage.getItem('hawaiiPrice')
+localStorage.getItem('vesuvioPrice')
+localStorage.getItem('margheritaPrice')
+
 let pepperoniPrice = document.getElementById('pepperoni-price').textContent
 let hawaiiPrice = document.getElementById('hawaii-price').textContent
 let vesuvioPrice = document.getElementById('vesuvio-price').textContent
-let marheritaPrice = document.getElementById('marherita-price').textContent
+let margheritaPrice = document.getElementById('margherita-price').textContent
 
 let pepperoni = document.getElementById('pepperoni')
 let hawaii = document.getElementById('hawaii')
 let vesuvio = document.getElementById('vesuvio')
-let marherita = document.getElementById('marherita')
+let margherita = document.getElementById('margherita')
 
-let totalPrice = document.getElementById('total-price')
+let totalPrice = document.getElementById('total-cost')
 
-
+let totalPriceValue = 0;
 
 
 
 pepperoni.addEventListener('click', () => {
-    totalPrice.innerText = 'Your total is: ' + pepperoniPrice
+    totalPriceValue += pepperoniPrice;
+    totalPrice.innerText = totalPriceValue;
+    localStorage.setItem('pepperoniPrice', pepperoniPrice)
 })
 
 hawaii.addEventListener('click', () => {
-    console.log('');
+    totalPriceValue += hawaiiPrice;
+    totalPrice.innerText = totalPriceValue;
+    localStorage.setItem('hawaiiPrice', hawaiiPrice)
+})
+
+vesuvio.addEventListener('click', () => {
+    totalPriceValue += vesuvioPrice;
+    totalPrice.innerText = totalPriceValue;
+    localStorage.setItem('vesuvioPrice', vesuvioPrice)
+})
+
+margherita.addEventListener('click', () => {
+    totalPriceValue += margheritaPrice;
+    totalPrice.innerText = totalPriceValue;
+    localStorage.setItem('margheritaPrice', margheritaPrice)
 })
